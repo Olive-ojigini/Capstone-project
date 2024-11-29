0a85128fbba5bf352ac1f6cc789d07d9978d4af5
oliveojigini <olivetochi6@gmail.com>
2024-11-29 12:54:23 +0100

    upgraded

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+const { applyTimestamps } = require("./Task");
 const schema=mongoose.schema;
 
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-} )
+  name: String,
+ refreshToken: String
+})
 
 
 module.exports = mongoose.model("User", UserSchema);
